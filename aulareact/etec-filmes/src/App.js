ce0ecd0c6d9a84395e6fdb5138871bca9323e6ef
BryanAlmeida016 <bryalmeida.moraes06@gmail.com>
2024-08-08 10:58:45 -0300

    Banner e Container

--- aulareact/etec-filmes/src/App.js
+++ aulareact/etec-filmes/src/App.js
@@ -1,11 +1,18 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       <Header/>
-      <h1>ETEC MCM</h1>
-      <p>Olá Mundo</p>
+      <Banner/>
+      <Container>
+        <h1>ETEC MCM</h1>
+        <p>Olá Mundo</p>
+      </Container>
+      <Footer/>
     </>
   );
 }
