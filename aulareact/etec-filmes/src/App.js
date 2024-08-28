dcbf9651495f680d6aafb38f62bb235cb4328382
BryanAlmeida016 <bryalmeida.moraes06@gmail.com>
2024-08-28 15:47:53 -0300

    Aulas 6 e 7

--- aulareact/etec-filmes/src/App.js
+++ aulareact/etec-filmes/src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Category, {categories, filterCategory} from "./components/Category";
 import Container from "./components/Container";
 
 function App() {
@@ -9,8 +11,34 @@ function App() {
       <Header/>
       <Banner image="home" />
       <Container>
-        <h1>ETEC MCM</h1>
-        <p>Olá Mundo! Vambora...</p>
+        
+        {categories.map((category, index) =>
+          <Category category={category}>
+            {filterCategory(index).map((video) => <Card id={video.id} key={video.id}/>)}
+          </Category>
+        )}
+
+        {/* <h2>Séries</h2>
+        <section className="cards">
+          {videos.map((video) => <Card id={video.id} key={video.id} />)}
+        </section> */}
+
+          {/* <Category category={categories[0]}>
+            {filterCategory(0).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[1]}>
+            {filterCategory(1).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[2]}>
+            {filterCategory(2).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[3]}>
+            {filterCategory(3).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[4]}>
+            {filterCategory(4).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> */}
+
       </Container>
       <Footer/>
     </>
